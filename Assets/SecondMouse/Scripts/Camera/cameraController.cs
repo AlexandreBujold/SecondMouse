c09eb5abe2f5d8bdf15da0f8e67a79d9304c82ad
mu_ki <duzkar@gmail.com>
2019-10-12 10:48:48 -0400

    CameraController init.

---
+++ Assets/SecondMouse/Scripts/Camera/cameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform cameraMountPoint;
+    public Transform lookAtTarget;
+    public float cameraTurnSpeed = 0.1f;
+    void LateUpdate()
+    {
+        
+        transform.rotation = Quaternion.Lerp(transform.rotation, transform.LookAt(lookAt), Time.time * cameraTurnSpeed);
+    }
+}

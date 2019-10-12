f63713dcddeaba596867305a17f2c407562a361e
Alexandre Bujold <alexandre.bujold@outlook.com>
2019-10-12 18:15:33 -0400

    Made pretty Camera and added pretty Skybox

---
+++ Assets/SecondMouse/Scripts/Course/Platform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Platform : MonoBehaviour
+{
+    public UnityEvent OnLanded;
+
+    [Space]
+    public bool fallOnLand;
+    public bool destroyOnLand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnLanded.AddListener(Landed);
+        OnLanded.Invoke();
+    }
+
+    public void RandomizeScale()
+    {
+
+    }
+
+    public void Fall()
+    {
+
+    }
+
+    public void Landed()
+    {
+
+    }
+}
